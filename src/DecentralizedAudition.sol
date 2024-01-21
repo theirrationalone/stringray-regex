bdbeeb34bfd6109cce7817107d4f70162876eb40
theirrationalone <anilkushwaha289@gmail.com>
2024-01-21 15:01:38 +0530

    lvl_2A

--- src/DecentralizedAudition.sol
+++ src/DecentralizedAudition.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.20;
 
 contract DecentralizedAudition {
-    error DecentralizedAudition__NameCountryStateAndEmailCannotBeEmpty();
+    error DecentralizedAudition__InvalidDataProvided(string invalidData);
     error DecentralizedAudition__ProposerAlreadyExists(string email);
+    error DecentralizedAudition__ProposerNotExists(string email);
 
     uint256 private s_proposerCounter;
 
@@ -41,7 +42,7 @@ contract DecentralizedAudition {
         string responsibility;
     }
 
-    struct Token {
+    struct TokenType {
         string name;
         string protocolName;
         string dexName;
@@ -70,7 +71,7 @@ contract DecentralizedAudition {
     struct Audit {
         string startDate;
         string endDate;
-        uint256 totalPricePoolUSD;
+        uint256 totalPrizePoolUSD;
         uint256 highAwardsUSD;
         uint256 mediumAwardsUSD;
         uint256 lowAwardsUSD;
@@ -81,13 +82,12 @@ contract DecentralizedAudition {
         uint256 numActors;
         uint256 numScopeSMCs;
         uint256 numDependencies;
-        uint256 numTokens;
+        uint256 numTokenTypes;
         uint256 numBlockchains;
         uint256 numKnownIssues;
         uint256 numAdditionalIssues;
+        uint256 numResources;
         string setupDetails;
-        string resourceLink;
-        string resourceTreeId;
     }
 
     mapping(address => mapping(string => AuditProposer)) private s_proposers;
@@ -95,10 +95,26 @@ contract DecentralizedAudition {
     mapping(address => mapping(uint256 => Actor)) private s_auditActors;
     mapping(address => mapping(uint256 => SMC)) private s_auditSmartContracts;
     mapping(address => mapping(uint256 => Dependency)) private s_auditDependencies;
-    mapping(address => mapping(uint256 => Token)) private s_auditTokens;
+    mapping(address => mapping(uint256 => TokenType)) private s_auditTokenTypes;
     mapping(address => mapping(uint256 => Blockchain)) private s_auditSupportedBlockchains;
     mapping(address => mapping(uint256 => KnownIssue)) private s_auditKnownIssues;
     mapping(address => mapping(uint256 => AdditionalIssue)) private s_auditAdditionalIssues;
+
+    modifier isValidStringData(string memory _strKey, string calldata _strVal) {
+        if (keccak256(abi.encodePacked(_strVal)) == keccak256(abi.encodePacked(""))) {
+            revert DecentralizedAudition__InvalidDataProvided(_strKey);
+        }
+
+        _;
+    }
+
+    modifier isValidUintData(string memory _uintKey, uint256 _uintVal) {
+        if (_uintVal <= 0) {
+            revert DecentralizedAudition__InvalidDataProvided(_uintKey);
+        }
+
+        _;
+    }
 
     function addProposer(
         string calldata _name,
@@ -107,21 +123,18 @@ contract DecentralizedAudition {
         string calldata _locationState,
         string calldata _contactInfo,
         string calldata _otherDetails
-    ) public {
-        if (
-            keccak256(abi.encodePacked(_name)) == keccak256(abi.encodePacked(""))
-                || keccak256(abi.encodePacked(_email)) == keccak256(abi.encodePacked(""))
-                || keccak256(abi.encodePacked(_locationCountry)) == keccak256(abi.encodePacked(""))
-                || keccak256(abi.encodePacked(_locationState)) == keccak256(abi.encodePacked(""))
-        ) {
-            revert DecentralizedAudition__NameCountryStateAndEmailCannotBeEmpty();
-        }
-
+    )
+        public
+        isValidStringData("name", _name)
+        isValidStringData("email", _email)
+        isValidStringData("country", _locationCountry)
+        isValidStringData("state", _locationState)
+    {
         if (s_proposers[msg.sender][_email].id != 0) {
             revert DecentralizedAudition__ProposerAlreadyExists(_email);
         }
 
-        AuditProposer({
+        AuditProposer memory proposer = AuditProposer({
             id: ++s_proposerCounter,
             name: _name,
             email: _email,
@@ -132,5 +145,76 @@ contract DecentralizedAudition {
             contactInfo: _contactInfo,
             otherDetails: _otherDetails
         });
+
+        s_proposers[msg.sender][_email] = proposer;
+    }
+
+    function addAudit(
+        string calldata _email,
+        string calldata _startDate,
+        string calldata _endDate,
+        uint256 _totalPrizePoolUSD,
+        uint256 _highAwardsUSD,
+        uint256 _mediumAwardsUSD,
+        uint256 _lowAwardsUSD,
+        uint256 _nSLOC,
+        uint256 _complxityScore,
+        uint256 _perComplexityUSD,
+        uint256 _perNSOLC_USD,
+        uint256 _numActors,
+        uint256 _numScopeSMCs,
+        uint256 _numDependencies,
+        uint256 _numTokenTypes,
+        uint256 _numBlockchains,
+        uint256 _numKnownIssues,
+        uint256 _numAdditionalIssues,
+        uint256 _numResources,
+        string calldata _setupDetails
+    )
+        public
+        isValidStringData("start date", _startDate)
+        isValidStringData("end date", _startDate)
+        isValidUintData("total USD pool prize", _totalPrizePoolUSD)
+        isValidUintData("high USD awards", _highAwardsUSD)
+        isValidUintData("medium USD awards", _mediumAwardsUSD)
+        isValidUintData("low USD awards", _lowAwardsUSD)
+        isValidUintData("nSLOC", _nSLOC)
+        isValidUintData("complexity score", _complxityScore)
+        isValidUintData("USD per complexity", _perComplexityUSD)
+        isValidUintData("USD per NSOLC", _perNSOLC_USD)
+        isValidUintData("number of actors", _numActors)
+        isValidUintData("number of Scoped SMCs", _numScopeSMCs)
+        isValidUintData("number of blockchains", _numBlockchains)
+        isValidUintData("num resources", _numResources)
+    {
+        AuditProposer memory proposer = s_proposers[msg.sender][_email];
+
+        if (proposer.id == 0) {
+            revert DecentralizedAudition__ProposerNotExists(_email);
+        }
+
+        Audit memory audit = Audit({
+            startDate: _startDate,
+            endDate: _endDate,
+            totalPrizePoolUSD: _totalPrizePoolUSD,
+            highAwardsUSD: _highAwardsUSD,
+            mediumAwardsUSD: _mediumAwardsUSD,
+            lowAwardsUSD: _lowAwardsUSD,
+            nSLOC: _nSLOC,
+            complxityScore: _complxityScore,
+            perComplexityUSD: _perComplexityUSD,
+            perNSOLC_USD: _perNSOLC_USD,
+            numActors: _numActors,
+            numScopeSMCs: _numScopeSMCs,
+            numBlockchains: _numBlockchains,
+            numDependencies: _numDependencies,
+            numTokenTypes: _numTokenTypes,
+            numKnownIssues: _numKnownIssues,
+            numAdditionalIssues: _numAdditionalIssues,
+            setupDetails: _setupDetails,
+            numResources: _numResources
+        });
+
+        s_proposedAudits[proposer.id][msg.sender] = audit;
     }
 }
