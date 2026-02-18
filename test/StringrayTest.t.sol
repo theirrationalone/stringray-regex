024cf370fb4f6e498157bd94715fb042a6a21c0e
theirrationalone <anilkushwaha289@gmail.com>
2026-02-18 19:36:53 +0000

    stringray regex all comprehensive testing --init🚀🚀🚀🚀🚀

---
+++ test/StringrayTest.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Stringray} from "../src/Stringray.sol";
+
+contract PlayStringTest is Test {
+    using Stringray for string;
+
+    function setUp() external {
+        // done
+    }
+}
