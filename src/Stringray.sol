3d67f6a4e640fc5739d9e628659d5284073d5f52
theirrationalone <anilkushwaha289@gmail.com>
2026-04-07 20:29:25 +0000

    BUG🐍: infinite trap found during word boundary impl

--- src/Stringray.sol
+++ src/Stringray.sol
@@ -1009,37 +1009,23 @@ contract Stringray {
                     matchEscapeLiteral(allAtoms[i].atom, stringInBytes, indexToStartMatch, isFirstMatch);
             } else if (allAtoms[i].atomType == WORD_BOUNDARY) {
                 (matchStartIndex, matchEndIndex) = matchWordBoundary(stringInBytes, indexToStartMatch);
-
-                console2.log("before update matchEndIndex: ", matchEndIndex);
-                // ##@anil!ak
-                // \b\banil\bak
-
+                console2.log("matchStartIndex: ", matchStartIndex);
+                console2.log("matchEndIndex: ", matchEndIndex);
+                console2.log("word boundary: ", wordBoundary);
                 if (matchEndIndex > -1) {
-                    if (matchStartIndex > -1) {
-                        if (firstIndex == -1) {
-                            firstIndex = matchStartIndex;
-                        }
+                    if (isFirstMatch) {
+                        indexToStartMatch = uint256(matchEndIndex);
+                        firstIndex = matchEndIndex;
                     } else {
-                        if (firstIndex == -1) {
-                            firstIndex = matchEndIndex;
+                        if (wordBoundary) {
+                            indexToStartMatch = uint256(matchEndIndex);
+                        } else {
+                            indexToStartMatch = matchStartIndex > -1 ? uint256(matchStartIndex) : uint256(matchEndIndex);
                         }
+                        matchEndIndex = matchStartIndex > -1 ? matchStartIndex : matchEndIndex;
                     }
 
-                    if (i > 0) {
-                        matchEndIndex = matchStartIndex;
-                    }
-
-                    indexToStartMatch = matchEndIndex == -1 ? indexToStartMatch : uint256(matchEndIndex);
                     wordBoundary = true;
-
-                    console2.log("matchStartIndex: ", matchStartIndex);
-                    console2.log("matchEndIndex: ", matchEndIndex);
-                    console2.log("firstIndex: ", firstIndex);
-                    console2.log("indexToStartMatch: ", indexToStartMatch);
-                    if (i == allAtoms.length - 1 && firstIndex > -1 && firstIndex == matchEndIndex) {
-                        matchEndIndex = -1;
-                    }
-
                     i++;
                     continue;
                 }
@@ -1047,8 +1033,8 @@ contract Stringray {
                 matchStartIndex = -1;
             }
 
-            console2.log("matchStartIndex: ", matchStartIndex);
-            console2.log("matchEndIndex: ", matchEndIndex);
+            console2.log("outside boundary matchStartIndex: ", matchStartIndex);
+            console2.log("outside boundary matchEndIndex: ", matchEndIndex);
 
             if (indexToStartMatch >= stringInBytes.length - 1) {
                 if (matchStartIndex == -1) {
@@ -1070,6 +1056,9 @@ contract Stringray {
                 matchStartIndex = 0;
                 matchEndIndex = -1;
 
+                console2.log("matchStartIndex: ", matchStartIndex);
+                console2.log("matchEndIndex: ", matchEndIndex);
+                console2.log("indexToStartMatch: ", indexToStartMatch);
                 i = 0;
                 continue;
             }
@@ -1089,7 +1078,7 @@ contract Stringray {
         for (uint256 i = indexToStartMatch; i < stringLength; i++) {
             if (isWord(stringInBytes[i], false)) {
                 if (i == 0) {
-                    return (0, 0);
+                    return (-1, 0);
                 }
 
                 if (i > 0 && !isWord(stringInBytes[i - 1], false)) {
